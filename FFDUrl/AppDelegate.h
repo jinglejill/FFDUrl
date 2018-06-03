76edfcce1f7081cd9dc0ac01ec9cecae00717ad6
Thidaporn Kijkamjai <jinglejill@me.com>
2018-06-04 01:16:52 +0700

    Initial Commit

---
+++ FFDUrl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFDUrl
+//
+//  Created by Thidaporn Kijkamjai on 4/6/2561 BE.
+//  Copyright © 2561 Appxelent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
